05e1d73d38a3a2d8c63236f86e88ab41dcacbe6d
VANBO\vanbo <van.bouwel.thomas@gmail.com>
2025-01-25 17:44:50 +0100

    bubble follows player

--- Assets/#Project/Scripts/Bubble.cs
+++ Assets/#Project/Scripts/Bubble.cs
@@ -6,7 +6,23 @@ public class Bubble : MonoBehaviour {
     public BlowForce blowForce;
     public BubbleColliderManager colliderManager;
 
+    private Transform _head;
+    
     public void Setup(Transform head) {
-        blowForce.Setup(head);
+        _head = head;
+        
+        blowForce.Setup(_head);
+    }
+
+    void Update() {
+        LookAtPlayer();
+    }
+
+    private void LookAtPlayer() {
+        var bubbleToHead = transform.position - _head.position;
+        var fwd = Vector3.Cross(Vector3.up, bubbleToHead).normalized; //face is on right so right needs to be fwd
+        var up = Vector3.Cross(bubbleToHead, fwd).normalized;
+        var targetRotation = Quaternion.LookRotation(fwd, up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 1.5f);
     }
 }
