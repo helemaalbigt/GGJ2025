965bca83691126937f0dd92cbdf30d098e9465b1
EMMY\caste <castelain.me@gmail.com>
2025-01-25 18:17:11 +0100

    letters title models

--- Assets/#Project/Scripts/TakingBubble.cs
+++ Assets/#Project/Scripts/TakingBubble.cs
@@ -1,23 +1,19 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class TakingBubble : MonoBehaviour
+public class TextureSwitcher : MonoBehaviour
 {
-
-    public Material BubbleMaterial;
-    public AudioSource audioSource;
-
-
-    public Texture textureWhenPlaying; 
-    public Texture textureWhenNotPlaying; 
+    public AudioSource audioSource;    // Reference to the AudioSource
+    public Texture textureWhenPlaying; // Texture to apply when the audio is playing
+    public Texture textureWhenNotPlaying; // Texture to apply when the audio is not playing
+    public float switchInterval = 3f;  // Interval in seconds between texture swaps
 
     private Renderer objectRenderer; 
+    private float timer = 0f;        
+    private bool usingTexture1 = true; 
 
     void Start()
     {
-       
+        // Get the Renderer component of the GameObject this script is attached to
         objectRenderer = GetComponent<Renderer>();
     }
 
@@ -27,11 +23,31 @@ public class TakingBubble : MonoBehaviour
         if (audioSource.isPlaying)
         {
            
-            objectRenderer.material.mainTexture = textureWhenPlaying;
+            timer += Time.deltaTime;
+
+            // Check if enough time has passed to switch the texture
+            if (timer >= switchInterval)
+            {
+                // Reset the timer
+                timer = 0f;
+
+                // Alternate between the two textures
+                if (usingTexture1)
+                {
+                    objectRenderer.material.mainTexture = textureWhenNotPlaying;
+                }
+                else
+                {
+                    objectRenderer.material.mainTexture = textureWhenPlaying;
+                }
+
+                // Toggle the flag for the next texture swap
+                usingTexture1 = !usingTexture1;
+            }
         }
         else
         {
-            
+            // If audio is not playing, ensure the material has the 'not playing' texture
             objectRenderer.material.mainTexture = textureWhenNotPlaying;
         }
     }
